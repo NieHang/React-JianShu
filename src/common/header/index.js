1de5fe288550207a002992a73faa0f2ff0973929
Bulb <1076883711@qq.com>
2018-08-19 16:56:59 +0800

    避免过多的Ajax请求，优化性能

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -25,7 +25,7 @@ import {
 
 class Header extends Component {
   render() {
-    const { focused, handleInputFocus, handleInputBlur } = this.props;
+    const { focused, list, handleInputFocus, handleInputBlur } = this.props;
 
     return (
       <HeaderWrapper>
@@ -52,7 +52,7 @@ class Header extends Component {
             <CSSTransition in={focused} timeout={500} classNames="slide">
               <NavSearch
                 className={focused ? "focused" : ""}
-                onFocus={handleInputFocus}
+                onFocus={() => {handleInputFocus(list)}}
                 onBlur={handleInputBlur}
               />
             </CSSTransition>
@@ -133,8 +133,8 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => {
   return {
-    handleInputFocus() {
-      dispatch(actionCreators.getList());
+    handleInputFocus(list) {
+      (list.size === 0) && dispatch(actionCreators.getList());
       dispatch(actionCreators.searchFocus());
     },
     handleInputBlur() {
