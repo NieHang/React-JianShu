84385c4f5a73049e008772ef1979e92234f2d12b
Bulb <1076883711@qq.com>
2018-08-20 11:15:30 +0800

    React路由Router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import React, { Component } from "react";
 import { Provider } from "react-redux";
+import { BrowserRouter, Route } from "react-router-dom";
 import Header from "./common/header";
 import store from "./store";
 
+/**
+ * React路由就是根据不同的url显示不同的内容
+ */
 class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <Header />
+        <div>
+          <Header />
+          {/* 使用路由 */}
+          <BrowserRouter>
+            <div>
+              {/* exact属性就是只匹配和path完全相同的路径 */}
+              <Route path="/" exact render={() => <div>home</div>} />
+              <Route path="/detail" exact render={() => <div>detail</div>} />
+            </div>
+          </BrowserRouter>
+        </div>
       </Provider>
     );
   }
