48288beab0601f57d2a5d5076621fb1a9fbd14a7
Bulb <1076883711@qq.com>
2018-08-28 21:03:26 +0800

    返回顶部功能实现

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,15 +1,20 @@
 import React, { Component } from "react";
-import { HomeWrapper, HomeLeft, HomeRight } from "./style";
-import { connect } from 'react-redux';
+import { HomeWrapper, HomeLeft, HomeRight, BackTop } from "./style";
+import { connect } from "react-redux";
 import List from "./components/List";
 import Recommend from "./components/Recommend";
 import Slider from "./components/Slider/index";
 import Topic from "./components/Topic";
 import Writer from "./components/Writer";
-import { actionCreators } from './store';
+import { actionCreators } from "./store";
 
 class Home extends Component {
+  handleScrollTop() {
+    window.scrollTo(0, 0);
+  }
+
   render() {
+    const { showScroll } = this.props;
     return (
       <HomeWrapper>
         <HomeLeft>
@@ -21,21 +26,45 @@ class Home extends Component {
           <Recommend />
           <Writer />
         </HomeRight>
+        {showScroll ? (
+          <BackTop onClick={this.handleScrollTop}>顶部</BackTop>
+        ) : null}
       </HomeWrapper>
     );
   }
 
   componentDidMount = () => {
     this.props.changeHomeData();
+    this.bindEvents();
+  };
+
+  componentWillUnmount() {
+    window.addEventListener("scroll", this.props.changeScrollTopShow);
   }
-  
+
+  bindEvents() {
+    window.addEventListener("scroll", this.props.changeScrollTopShow);
+  }
 }
+
+const mapState = state => ({
+  showScroll: state.getIn(["home", "showScroll"])
+});
 
 const mapDispatchToProps = dispatch => ({
   changeHomeData() {
-    const action = actionCreators.getHomeInfo();
-    dispatch(action);
+    dispatch(actionCreators.getHomeInfo());
+  },
+  changeScrollTopShow() {
+    if (document.documentElement.scrollTop > 400) {
+      dispatch(actionCreators.toggleTopShow(true));
+    } else {
+      dispatch(actionCreators.toggleTopShow(false));
+    }
   }
-})
+});
 
-export default connect(null, mapDispatchToProps)(Home);
+export default connect(
+  mapState,
+  mapDispatchToProps
+)(Home);
--- src/pages/home/store/reducer.js
+++ src/pages/home/store/reducer.js
@@ -8,23 +8,34 @@ const defaultState = fromJS({
   articleList: [],
   recommendList: [],
   writer: [],
-  articlePage: 1
+  articlePage: 1,
+  showScroll: false
 });
+
+const changeHomeData = (state, action) => {
+  return state.merge({
+    topicList: action.topicList,
+    articleList: action.articleList,
+    recommendList: action.recommendList,
+    writer: action.writer
+  });
+};
+
+const addArticleList = (state, action) => {
+  return state.merge({
+    articleList: state.get("articleList").concat(action.articleList),
+    articlePage: action.nextPage
+  });
+};
 
 export default (state = defaultState, action) => {
   switch (action.type) {
     case constants.CHANGE_HOME_DATA:
-      return state.merge({
-        topicList: action.topicList,
-        articleList: action.articleList,
-        recommendList: action.recommendList,
-        writer: action.writer
-      });
+      return changeHomeData(state, action);
     case constants.ADD_ARTICLE_LIST:
-      return state.merge({
-        articleList: state.get("articleList").concat(action.articleList),
-        articlePage: action.nextPage
-      });
+      return addArticleList(state, action);
+    case constants.TOGGLE_SCROLL_TOP:
+      return state.set("showScroll", action.show);
     default:
       return state;
   }
